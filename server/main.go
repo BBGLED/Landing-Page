0ee6e19170078418a616e72711867244f3e03201
Glendale Acosta <gglendale17@gmail.com>
2020-02-28 15:10:33 -0800

    Use env for heroku

--- server/main.go
+++ server/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+        "github.com/joho/godotenv"
+        "log"
         "net/http"
+        "os"
 )
 
+func LoadEnv() {
+        err := godotenv.Load()
+        if err != nil {
+                log.Fatal("Error loading .env file")
+        }
+}
+
 func main() {
+        LoadEnv()
+        PORT := os.Getenv("PORT")
+
         http.Handle("/", http.FileServer(http.Dir("./public")))
-        http.ListenAndServe(":3000", nil)
+        http.ListenAndServe(PORT, nil)
 }
