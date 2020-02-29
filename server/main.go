a4655d592685ea504fa4fb4c9bcee321202bc735
Glendale Acosta <gglendale17@gmail.com>
2020-02-28 19:25:56 -0800

    test

--- server/main.go
+++ server/main.go
@@ -2,22 +2,20 @@ package main
 
 import (
         "fmt"
-        "github.com/joho/godotenv"
-        "log"
         "net/http"
         "os"
 )
 
-func LoadEnv() {
-        err := godotenv.Load()
-        if err != nil {
-                log.Fatal("Error loading .env file")
-        }
-}
+//func LoadEnv() {
+        //err := godotenv.Load()
+        //if err != nil {
+        //        log.Fatal("Error loading .env file")
+        //}
+//}
 
 func main() {
         fmt.Println("RUNNING MAIN, WOO")
-        LoadEnv()
+        //LoadEnv()
         PORT := os.Getenv("PORT")
         fmt.Printf("TESTING PORT ON HEROKU: %s!", PORT)
         http.Handle("/", http.FileServer(http.Dir("./public")))
