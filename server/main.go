d771a35a1acc9299cadf203e2510358ee64cc2e5
Glendale Acosta <gglendale17@gmail.com>
2020-02-29 02:42:33 -0800

    Use gin and refactor

--- server/main.go
+++ server/main.go
@@ -2,24 +2,24 @@ package main
 
 import (
         "fmt"
+        "github.com/gin-gonic/gin"
         "net/http"
         "os"
 )
 
-//func LoadEnv() {
-        //err := godotenv.Load()
-        //if err != nil {
-        //        log.Fatal("Error loading .env file")
-        //}
-//}
+func GetPORT() string {
+        PORT := os.Getenv("PORT")
+        if PORT == "" {
+                PORT = "3001"
+        }
+        return ":" + PORT
+}
 
 func main() {
-        fmt.Println("RUNNING MAIN, WOO")
-        //LoadEnv()
-        PORT := os.Getenv("PORT")
-        fmt.Printf("TESTING PORT ON HEROKU: %s!", PORT)
-        http.Handle("/", http.FileServer(http.Dir("./public")))
+        PORT := GetPORT()
+        router := gin.Default()
+        router.StaticFS("../", http.Dir("public"))
 
         fmt.Printf("App running on port %s!", PORT)
-        http.ListenAndServe(":"+PORT, nil)
+        router.Run(PORT)
 }
